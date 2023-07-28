b38dcf73f7b5155d882b0589c5dca3df20c45bda
BadeaAlex <123966802+BadeaAlex@users.noreply.github.com>
2023-07-28 11:36:26 +0100

    publishing

---
+++ ageSample.java
@@ -0,0 +1,18 @@
+package ageSample;
+
+public class ageSample {
+    public void mathSample() {
+    
+    }
+        static void main(String sName, int iAge) {
+            System.out.println("<<<<<----------------------->>>>>");
+            System.out.println(sName + " is " + iAge + " years old.");
+
+        }
+        public static void main(String[] args) {
+            main("Alex", 23);
+            main("George", 30);
+            main("Aiko", 25);
+        }
+
+}
